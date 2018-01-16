bf345357db945ed54b29761e184ec35d53ff3c1f
Akshay Iyer <c.m.akshay.iyer@gmail.com>
2018-01-16 13:18:00 +0530

    Add vue life cycle hooks and data object

---
+++ Chapter2/product-data.js
@@ -0,0 +1,15 @@
+const data = {
+  data: {
+    sitename: 'Vue.js Pet Depot',
+    product: {
+      id: 1001,
+      title: 'Cat Food, 25lb bag',
+      description: 'A 25 pound bag of <em>irresistible</em>, ' +
+    'organic goodness for your cat.',
+      price: 2000,
+      image: 'assets/images/product-fullsize.png'
+    }
+  }
+}
+
+exports = data
