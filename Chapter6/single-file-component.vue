21d048de7f0e2d1bade542a9b98f9edc89f98de4
Akshay Iyer <c.m.akshay.iyer@gmail.com>
2018-01-17 11:04:40 +0530

    Add single file component

---
+++ Chapter6/single-file-component.vue
@@ -0,0 +1,20 @@
+<template>
+ <div class="hello">
+   {{msg}}
+ </div>
+</template>
+
+<script>
+export default {
+  name: 'hello',
+  data () {
+    return {
+      msg: 'Welcome to your Vue.js App'
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
