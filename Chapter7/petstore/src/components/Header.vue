cf3c9f2cbf29f200383f087fc3eba3cd2a28d461
Akshay Iyer <c.m.akshay.iyer@gmail.com>
2018-01-19 15:58:20 +0530

    Add pet store project

---
+++ Chapter7/petstore/src/components/Header.vue
@@ -0,0 +1,36 @@
+<template>
+  <header>
+    <div class="navbar navbar-default">
+     <div class="navbar-header">
+        <h1><router-link :to="{name: 'Main'}">{{ sitename }}</router-link></h1>
+     </div> 
+    <div class="nav navbar-nav navbar-right cart">
+      <button type="button" class="btn btn-default btn-lg" v-on:click="showCheckout">
+        <span class="glyphicon glyphicon-shopping-cart">{{ cartItemCount }}</span>
+      </button>
+    </div>
+    </div>
+  </header>
+</template>
+<script>
+export default {
+  name: 'hello',
+  data () {
+    return {
+      sitename: 'Vue.js Pet Depot'
+    }
+  },
+  props: ['cartItemCount'],
+  methods: {
+    showCheckout () {
+      this.$router.push({name: 'Form'})
+    }
+  }
+}
+</script>
+<style scoped>
+a {
+  text-decoration: none;
+  color: black;
+}
+</style>
